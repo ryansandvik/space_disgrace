75d398fdc88298fee61c92bfccbcdb8e043d245c
ryansandvik <ryansandvik@gmail.com>
2018-07-10 13:30:50 -0700

    added first map, rocket, thrust // control script

---
+++ Space_Disgrace/Assets/Prefabs/Rocket/Scripts/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            print("Left");
+        }
+        else if (Input.GetKey(KeyCode.R))
+        {
+            print("Right");
+        }
+    }
+}
