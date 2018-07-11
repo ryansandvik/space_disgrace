e77e709d2b66b723164f3cac9d573b759adb3121
ryansandvik <ryansandvik@gmail.com>
2018-07-10 18:29:05 -0700

    added audio, refined movement

--- Space_Disgrace/Assets/Prefabs/Rocket/Scripts/Rocket.cs
+++ Space_Disgrace/Assets/Prefabs/Rocket/Scripts/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start ()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -24,14 +26,22 @@ public class Rocket : MonoBehaviour {
         if(Input.GetKey(KeyCode.E))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.W))
         {
-            print("Left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.R))
         {
-            print("Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
